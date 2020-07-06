77c3975f49ad68ddf43a9a238cbcbc95981a649a
Dominika Dyrcz-Ciesielska <ciesielski.michal@interia.eu>
2020-07-07 00:19:57 +0200

    Updates

---
+++ precache-manifest.04ff391ff2277e899bb31829d74d86c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9f27f2e300513e20e3d2d1627dba3a",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "46a832fc624552b0a63e",
+    "url": "/Weather-app/static/css/main.78a05461.chunk.css"
+  },
+  {
+    "revision": "b1e610564bb9f396c1a6",
+    "url": "/Weather-app/static/js/2.01e6ea29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.01e6ea29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a832fc624552b0a63e",
+    "url": "/Weather-app/static/js/main.38a1e0fb.chunk.js"
+  },
+  {
+    "revision": "a2861103e0194e8c138c",
+    "url": "/Weather-app/static/js/runtime-main.1d012fff.js"
+  },
+  {
+    "revision": "afab29daf47ad4934c078ed73624fed5",
+    "url": "/Weather-app/static/media/background.afab29da.jpg"
+  }
+]);
